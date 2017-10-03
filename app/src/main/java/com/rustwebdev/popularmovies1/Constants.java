f03b0a1ff63bfe1b2f504cf1f37c8a712a2e981a
rustwebdev <rustwebdev@gmail.com>
2017-10-02 21:03:39 -0600

    Delete Constants.java
    oops!

--- app/src/main/java/com/rustwebdev/popularmovies1/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.rustwebdev.popularmovies1;
-
-public class Constants {
-  public static final String BASE_URL = "https://api.themoviedb.org/3/discover/";
-  public static final String API_KEY = "c0c4c4c8849992b57f4fabd69acf6bf2";
-  public static final String SORT_POPULAR = "popularity.desc";
-  public static final String SORT_RATING = "vote_average.desc";
-}
