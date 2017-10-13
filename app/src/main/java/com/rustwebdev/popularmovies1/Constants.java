caee8f48f22cee3195d566ea8f169773c016194e
Daniel Rust <rustwebdev@gmail.com>
2017-10-13 16:37:39 -0600

    Ooops!

--- app/src/main/java/com/rustwebdev/popularmovies1/Constants.java
+++ app/src/main/java/com/rustwebdev/popularmovies1/Constants.java
@@ -2,7 +2,7 @@ package com.rustwebdev.popularmovies1;
 
 public class Constants {
   public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
-  public static final String API_KEY = "c0c4c4c8849992b57f4fabd69acf6bf2";
+  public static final String API_KEY = "";
   public static final String SORT_POPULAR = "popular";
   public static final String SORT_RATING = "top_rated";
   public static final String IMG_PATH = "http://image.tmdb.org/t/p/w185/";
