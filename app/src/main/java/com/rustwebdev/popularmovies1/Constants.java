93b1db533bf4f6fe813ea61b2c484c0e149681f0
Daniel Rust <rustwebdev@gmail.com>
2017-10-16 23:54:52 -0600

    [Changed ConstraintLayout v to 1.0.2] Fixed ScrollView/RecyclerView issue with NestedScrollView, Added multiple trailers to MovieActivity, and converted MovieActivity to Constraint Layout.

--- app/src/main/java/com/rustwebdev/popularmovies1/Constants.java
+++ app/src/main/java/com/rustwebdev/popularmovies1/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
   public static final String VOTE_AVERAGE_CONCAT = "/10";
   public static final String MOVIE_IMG_TRANS_SHARED_ELEMENT = "movieImgTrans";
   public static final String RETROFIT_ERROR_MESSAGE =
-      "We're sorry, the service is unavailable right now. Please try again later.";
+      "We're sorry, this service is unavailable right now. Please try again later.";
   public static final String YOUTUBE_URL_PREFIX = "http://www.youtube.com/watch?v=";
 }
 
